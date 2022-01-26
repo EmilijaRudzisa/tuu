646cde8e782dce2f077b768b340ab687f99efb78
Emily <97593214+EmilijaRudzisa@users.noreply.github.com>
2022-01-26 10:07:49 +0000

    iytt

---
+++ script.js
@@ -0,0 +1,32 @@
+function fetchData() {
+  fetch("./data2.json").then(response => {
+    if (!response.ok) {
+      throw Error("Kļūda!");
+    }
+
+    return response.json();
+  })
+
+  .then(data => {
+    const tabula = data.map(persona => {
+      return `
+          <tr>
+            <td>${persona.Gramatas ID}</td>
+            <td>${persona.Nosaukums}</td>
+            <td>${persona.Autors}</td>
+            <td>${persona.Valoda}</td>
+          </tr>
+      `;
+    })
+    .join("");
+
+    document.querySelector('#showdata').innerHTML = tabula;
+  })
+
+  .catch(error => {
+    console.log(error);
+  });
+}
+
+fetchData();
+
